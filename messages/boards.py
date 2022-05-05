1074652b95227a733ab9d18db4d02cd1b04c9957
simplejoshua <75175923+simplejoshua@users.noreply.github.com>
2022-05-05 09:04:57 +0800

    Local Leaderboard Update 2.1
    Added boards.py on messages folder for the congratulatory messages

---
+++ messages/boards.py
@@ -0,0 +1,3 @@
+
+GOT_IN_LB = "🎉 Congratulations, {0.mention}! You've earned a spot on the LOCAL Leaderboard. **s!localboard** to see your rank in this server's leaderboard."
+GOT_IN_GB = "🏆 Woohoo, {0.mention}! You've earned a spot on the GLOBAL Leaderboard. **s!globalboard** to see how well you did among all stock traders in the Discord universe."
